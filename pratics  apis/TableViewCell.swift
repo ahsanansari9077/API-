4947e41f17b28a989ae3dab4652d3d3c60a6c3a1
ahsanansari9077 <61463845+ahsanansari9077@users.noreply.github.com>
2020-02-26 04:04:33 -0800

    once again

--- pratics  apis/TableViewCell.swift
+++ pratics  apis/TableViewCell.swift
@@ -13,7 +13,7 @@ class TableViewCell: UITableViewCell {
     @IBOutlet weak var namelbl: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+ 
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
